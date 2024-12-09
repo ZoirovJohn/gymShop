d750bae195c758ebeffd9bd629ce0cd5e1914cd6
Thomas <zoirovjohn@gmail.com>
2024-12-10 08:50:58 +0900

    feat: connect mongoDB via mongoose

--- src/train.ts
+++ src/train.ts
@@ -1,2 +1,9 @@
 console.log("Train.ts ishga tushdi");
 
+// H-TASK
+
+function getPositive(arr: Array<number>) {
+    return arr.filter(ele => ele > 0 ? ele : null).join("")
+}
+
+console.log(getPositive([1, -4, 2]));
--- train2.js
+++ train2.js
@@ -1,14 +1,4 @@
 
 
 
-// if "me"  =>  ["home", "milk", "Mercury", "fish"], the method should return ["home", "Mercury"].
 
-
-
-
-function wordSearch(query, seq){
-    const result = seq.filter(ele => ele.toLowerCase().includes(query.toLowerCase()))
-    return result.length == 0 ? ["Empty"] : result;
-}
-
-console.log(wordSearch("abcd", ["za", "aB", "Abc", "zAB", "zbc"]))
