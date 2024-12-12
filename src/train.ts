c441d720c6885d457b20fe6b8b481a6dd42dd8be
Thomas <zoirovjohn@gmail.com>
2024-12-12 12:03:29 +0900

    feat: develop member schema model

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,27 @@
 console.log("Train.ts ishga tushdi");
 
-// H-TASK
+// H2-TASK: 
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+
+function getDigits(input: String) {
+    let result:String = "";
+    for (const ele of input) {
+        if (!isNaN(Number(ele))) {
+            result += ele;
+        }
+    }
+    return result
+}
+
+console.log(getDigits("m14i1t"));
+
+// ---------------------------------------------------
+/*// H-TASK
 
 function getPositive(arr: Array<number>) {
     return arr.filter(ele => ele > 0 ? ele : null).join("")
 }
 
 console.log(getPositive([1, -4, 2]));
+*/
