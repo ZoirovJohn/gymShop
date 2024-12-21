4dd96ab077f4f96ab6a020bdded297f4b5d22a0c
Thomas <zoirovjohn@gmail.com>
2024-12-21 16:21:15 +0900

    fix: organize spa & ssr business logics

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -11,6 +11,48 @@ class MemberService {
         this.memberModel = MemberModel
     }
 
+    /** SPA */
+    
+    public async signup(input: MemberInput): Promise<Member> {
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+
+        try {
+            const result = await this.memberModel.create(input); // db da inputni create qiladi            
+            result.memberPassword = "";
+            return result.toJSON();
+        } catch (err) {
+            console.log("Error, modelsignup", err);
+            throw new Errors(HttpCode.BAD_REQUEST, Message.USED_NICK_PHONE);
+        }
+    }
+
+    public async login(input: LoginInput): Promise<Member> {
+        // TODO: Consider member status later
+        const member = await this.memberModel
+            .findOne(
+                { memberNick: input.memberNick },
+                { memberNick: 1, memberPassword: 1 }
+            )
+            .exec();
+        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+        const isMatch = await bcrypt.compare(
+            input.memberPassword,
+            member.memberPassword
+        );
+
+
+        if (!isMatch) {
+            throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD)
+        }    
+
+        //lean() - db dan olingan mantiq qkncalik ozgartrish imkoniyatini beradi
+        return await this.memberModel.findById(member._id).lean().exec();
+    }
+
+    /** SSR */
+
     public async processSignup(input: MemberInput): Promise<Member> {
         const exist = await this.memberModel
             .findOne({ memberType: MemberType.RESTAURANT })
@@ -42,18 +84,18 @@ class MemberService {
             )
             .exec();
         if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
-        
+
         // hash qiganda, qande salt bn hash qilingani yoziladi. Kn compareda usha salt orqali topvaladi original datani
         const isMatch = await bcrypt.compare(
             input.memberPassword,
             member.memberPassword
         );
         // const isMatch = input.memberPassword === member.memberPassword;
-        
 
-        
-        if(!isMatch) throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD)
-        
+
+
+        if (!isMatch) throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD)
+
         return await this.memberModel.findById(member._id).exec();
     }
 }
