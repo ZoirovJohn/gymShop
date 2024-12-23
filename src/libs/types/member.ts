74a34e099a75b4e8006ea16906a83765efdc63f9
Thomas <zoirovjohn@gmail.com>
2024-12-24 07:30:35 +0900

    fix: create sessions on login and singup process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,5 +1,7 @@
 import { ObjectId } from "mongoose";
 import { MemberStatus, MemberType } from "../enums/member.enum";
+import { Request } from "express";
+import { Session } from "express-session";
 
 // Member -bu db dan qaytekan data uchun type
 export interface Member {
@@ -32,4 +34,9 @@ export interface MemberInput {
 export interface LoginInput {
     memberNick: string;
     memberPassword: string;
+}
+
+export interface AdminRequest extends Request {
+    member: Member;
+    session: Session & { member: Member }
 }
