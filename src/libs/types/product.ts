c231912880dbf5141b0bde6af1a9ea9e519c1507
Thomas <zoirovjohn@gmail.com>
2025-01-30 19:05:54 +0900

    feat: develop getProducts rest Api

--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -1,49 +1,56 @@
 import { ObjectId } from "mongoose";
 
 import {
-    ProductCollection,
-    ProductSize,
-    ProductStatus,
+  ProductCollection,
+  ProductSize,
+  ProductStatus,
 } from "../enums/product.enum";
 
 export interface Product {
-    _id: ObjectId,
-    productStatus: ProductStatus;
-    productCollection: ProductCollection;
-    productName: string;
-    productPrice: number;
-    productLeftCount: number;
-    productSize?: ProductSize;
-    productVolume: number;
-    productDesc?: string;
-    productImages: string[];
-    productViews?: number;
+  _id: ObjectId;
+  productStatus: ProductStatus;
+  productCollection: ProductCollection;
+  productName: string;
+  productPrice: number;
+  productLeftCount: number;
+  productSize?: ProductSize;
+  productVolume: number;
+  productDesc?: string;
+  productImages: string[];
+  productViews?: number;
 }
 
+export interface ProductInquiry {
+  order: string;
+  page: number;
+  limit: number;
+  productCollection?: ProductCollection;
+  search?: string;
+}
 
 export interface ProductInput {
-    productStatus?: ProductStatus;
-    productCollection: ProductCollection;
-    productName: string;
-    productPrice: number;
-    productLeftCount: number;
-    productSize?: ProductSize;
-    productVolume?: number;
-    productDesc?: string;
-    productImages?: string[];
-    productViews?: number;
+  productStatus?: ProductStatus;
+  productCollection: ProductCollection;
+  productName: string;
+  productPrice: number;
+  productLeftCount: number;
+  productSize?: ProductSize;
+  productVolume?: number;
+  productDesc?: string;
+  productImages?: string[];
+  productViews?: number;
 }
 
 export interface ProductUpdateInput {
-    _id: ObjectId,
-    productStatus?: ProductStatus;
-    productCollection?: ProductCollection;
-    productName?: string;
-    productPrice?: number;
-    productLeftCount?: number;
-    productSize?: ProductSize;
-    productVolume?: number;
-    productDesc?: string;
-    productImages?: string[];
-    productViews?: number;
+  _id: ObjectId;
+  productStatus?: ProductStatus;
+  productCollection?: ProductCollection;
+  productName?: string;
+  productPrice?: number;
+  productLeftCount?: number;
+  productSize?: ProductSize;
+  productVolume?: number;
+  productDesc?: string;
+  productImages?: string[];
+  productViews?: number;
 }
